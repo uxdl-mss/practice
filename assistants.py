3619f7beb90ee6e3c75cc031d5ac59940bc76180
Prime69420 <sivaramasarma9999@gmail.com>
2025-11-14 18:23:06 +0530

    created basic assistant, thread, message and run. with streaming.

--- assistants.py
+++ assistants.py
@@ -1,6 +1,5 @@
 from openai import AzureOpenAI
 import os
-import time
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -17,13 +16,35 @@ deployment = os.getenv("deployment")
 
 
 assistant = client.beta.assistants.create(
-    name="ThreadDemoAssistant",
-    instructions=(
-        "You are a helpful assistant"
-    ),
+    name="assistant1",
+    instructions="You are a helpful assistant.",
     model=deployment
 )
-
 print("Assistant created:", assistant.id)
 
 
+thread = client.beta.threads.create()
+print("Thread created:", thread.id)
+
+
+client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="user",
+    content="whats 1+1"
+)
+
+
+print("\n \n Streaming Output: \n")
+
+with client.beta.threads.runs.stream(
+    thread_id=thread.id,
+    assistant_id=assistant.id
+) as stream:
+
+    for event in stream:
+        if event.event == "response.output_text.delta":
+            print(event.delta, end="", flush=True)
+
+    final_run = stream.get_final_run()
+
+print("\n\nRun finished:", final_run.status)
